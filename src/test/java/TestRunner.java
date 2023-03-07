02d251037dc1de9146bb65e19624e60e2ddcf471
TarasYanUA <wildmonach@gmail.com>
2023-03-07 17:21:44 +0200

    Created a structure of classes.

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+/*
+Модуль "SEO для товарных вкладок" + UniTheme2(UltRu) 4.16.1b.
+Запускать через файл TestNG.xml
+Скриншоты смотреть в папке: reports -> tests
+ */
+
+public class TestRunner {
+    public static final String BASIC_URL = "https://trs.test.abt.team/4161ultru/admin.php";
+
+    @BeforeClass
+    public void openBrowser() {
+        Configuration.browser = "chrome";
+        Configuration.holdBrowserOpen = false; //не закрываем браузер пока ведём разработку
+        Configuration.screenshots = true; //делаем скриншоты при падении
+        Configuration.browserSize = "1920x1050"; //Увеличиваем размер экрана
+        open(BASIC_URL);
+        $(".btn.btn-primary").click();
+        $("#bp_off_bottom_panel").click();
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+
+    public void selectLanguage_RTL() {
+        $("a[id*='_wrap_language_']").hover().click();
+        $(".ty-select-block__list-item a[data-ca-name='ar']").click();
+    }
+    public void selectLanguage_RU() {
+        $("a[id*='_wrap_language_']").hover().click();
+        $(".ty-select-block__list-item a[data-ca-name='ru']").click();
+    }
+}

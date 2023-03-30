40c8663c2d753246aa4039ac78cf1d88fcf08daf
TarasYanUA <wildmonach@gmail.com>
2023-03-30 15:05:37 +0300

    Fixed some locators.

--- src/main/java/adminPanel/SeoTabsSettings.java
+++ src/main/java/adminPanel/SeoTabsSettings.java
@@ -8,6 +8,6 @@ public class SeoTabsSettings {
     public SeoTabsSettings(){super();}
     public SelenideElement button_SaveSettings = $(".cm-addons-save-settings");
     public SelenideElement tab_Settings = $("#settings");
-    public SelenideElement setting_AddNavigationPanel = $(By.id("addon_option_ab__seo_product_tabs_add_tabs_fixed_panel_5951"));
-    public SelenideElement setting_PositionOfNavigationPanel = $(By.id("addon_option_ab__seo_product_tabs_tabs_fixed_panel_position_5952"));
+    public SelenideElement setting_AddNavigationPanel = $(By.cssSelector("input[id*='addon_option_ab__seo_product_tabs_add_tabs_fixed_panel']"));
+    public SelenideElement setting_PositionOfNavigationPanel = $(By.cssSelector("select[id*='addon_option_ab__seo_product_tabs_tabs_fixed_panel_position']"));
 }
